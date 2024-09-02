43d2adf688aba95e6eee644e253ed9a9d349ea75
aminhp93 <minhpn.org.ec@gmail.com>
2024-09-02 14:16:50 +0700

    [86cvwe8be] Update structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import NextFederationPlugin from "@module-federation/nextjs-mf";
+
+const nextConfig = {
+  reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "container",
+        remotes: {
+          // container: `next1@http://localhost:3001/_next/static/${
+          //   isServer ? "ssr" : "chunks"
+          // }/remoteEntry.js`,
+        },
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./index": "./pages/index",
+          // "./checkout": "./pages/checkout",
+        },
+        shared: {
+          // whatever else
+        },
+      })
+    );
+
+    return config;
+  },
+};
 
 export default nextConfig;
