9ba83cc67b3a23304ec125c26b9bb47af76cdfab
aminhp93 <minhpn.org.ec@gmail.com>
2024-09-15 11:25:15 +0700

    [86cvwe8be] add blog detail content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <div>Hello blog</div>;
+import dynamic from "next/dynamic";
+
+const Blog = dynamic(() => import("@/features/blog"), {
+  ssr: false,
+});
+
+export default function BlogPage() {
+  return <Blog />;
 }
